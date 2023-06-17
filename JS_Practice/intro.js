2ee29dceea247f69ff8ed406c68d8f42aee5c816
Sharma <divyangi.sharma@dxc.com>
2023-06-17 13:12:02 +0530

    JavaScript

---
+++ JS_Practice/intro.js
@@ -0,0 +1,8 @@
+function company()
+            {
+                document.getElementById("myImage").src = "aws.png"
+            }
+function place()
+            {
+                document.getElementById("myImage").src = "italy.png"
+            }
