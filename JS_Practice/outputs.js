0ba9962ebdbfb66c9d78cd5b39023af5af739d07
Sharma <divyangi.sharma@dxc.com>
2023-06-17 13:34:38 +0530

    javascript

---
+++ JS_Practice/outputs.js
@@ -0,0 +1,11 @@
+//WRITE METHOD
+document.write("this works ", 5*6)
+
+//innerHTML METHOD
+document.getElementById("demo").innerHTML="this works too"
+
+//ALERT METHOD
+window.alert("hey, its an alert!!")
+
+//CONSOLE METHOD
+console.log("this worked in the console",27+19)
